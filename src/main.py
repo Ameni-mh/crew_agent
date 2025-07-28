68491f1d492b92ef8efdbf80764b5a7488376c02
Ameni-mh <ameni.m@mediastorykw.com>
2025-07-28 11:54:29 +0100

    add asyn mode in hotel_selector endpoint

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ from Agent.lookup_hotels import search_queries_recommendation_agent, Extract_fil
 from Tool.tool import detect_language_tool, search_hotels_from_GDSAgregator, validate_field_tool
 from Agent.select_option_agent import Hotel_selector_room_booking_agent, Hotel_selector_room_booking_task
 from Tool.redis_tool import change_option_status_hotel_offer, get_all_rooms_from_key, get_room_search_payload_from_key, get_selected_rooms_from_key, is_selected_option_from_key, save_hotelDetails_room_options
+import asyncio
 
 app = FastAPI()
 
@@ -70,56 +71,71 @@ async def lookup_hotels(query: str):
 
 @base_router.post("/hotel_selector")
 async def hotel_selector(query: str, convo_id, user_id):
+    try:
 
-    about_company = "Vialink is a company that provides AI solutions to help Travels booking hotel, flighet."
+        about_company = "Vialink is a company that provides AI solutions to help Travels booking hotel, flighet."
 
-    company_context = StringKnowledgeSource(
-        content=about_company
-    )
+        company_context = StringKnowledgeSource(
+            content=about_company
+        )
 
-    tools_list = [
-        get_all_rooms_from_key, #
-        detect_language_tool, #
-        save_hotelDetails_room_options, 
-        change_option_status_hotel_offer, 
-        is_selected_option_from_key, #
-        get_room_search_payload_from_key, 
-        get_selected_rooms_from_key]
-    tool_names = [tool.name for tool in tools_list]
+        tools_list = [
+            get_all_rooms_from_key, #
+            detect_language_tool, #
+            save_hotelDetails_room_options, 
+            change_option_status_hotel_offer, 
+            is_selected_option_from_key, #
+            get_room_search_payload_from_key, 
+            get_selected_rooms_from_key]
+        tool_names = [tool.name for tool in tools_list]
+        
+
+        agentops.init(
+        api_key=settings.agentops_api_key,
+        skip_auto_end_session=True,
+        default_tags=['crewai']
+        )
     
-
-    agentops.init(
-    api_key=settings.agentops_api_key,
-    skip_auto_end_session=True,
-    default_tags=['crewai']
-    )
-  
-    rankyx_crew = Crew(
-    agents=[
-        Hotel_selector_room_booking_agent 
-    ],
-    tasks=[
-        Hotel_selector_room_booking_task,
+        rankyx_crew = Crew(
+        agents=[
+            Hotel_selector_room_booking_agent 
         ],
-    process=Process.sequential,
-    knowledge_sources=[company_context]
-)
-    crew_results = rankyx_crew.kickoff(
-    inputs={
-        "user_id": user_id,
-        "convo_id": convo_id,
-        "tool_names": tool_names,
-        "input": query ,
+        tasks=[
+            Hotel_selector_room_booking_task,
+            ],
+        process=Process.sequential,
+        knowledge_sources=[company_context]
+    )
+        
+        
+        
+        crew_results = await rankyx_crew.kickoff_async(
+            inputs={
+                "user_id": user_id,
+                "convo_id": convo_id,
+                "tool_names": tool_names,
+                "input": query ,
 
-    })
+            })
 
-    result = crew_results.to_dict()
+        result = crew_results.to_dict()
 
-    return JSONResponse(
+        
+        return JSONResponse(
             content={
                 "signal": "success",
                 "result": result if result else "No results found",
-            }
+                    }
+                )
+        
+        
+    except Exception as e:
+        return JSONResponse(
+            content={
+                "signal": "error",
+                "message": str(e)
+            },
+            status_code=500
         )
 
 
