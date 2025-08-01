db1614e08e50a680e2245c9b00597074b36afdb6
Ameni-mh <ameni.m@mediastorykw.com>
2025-08-01 12:21:54 +0100

    update variable name

--- src/Tool/room_tool.py
+++ src/Tool/room_tool.py
@@ -1,10 +1,9 @@
-from Tool.redis_tool import change_option_status_hotel_offer, get_room_search_payload_from_key, is_selected_option_from_key, save_hotelDetails_room_options
-from Tool.gds_hotel_service import search_details_specific_hotel
+from Tool.redis_tool import save_hotelDetails_room_options
 from crewai.tools import tool
-from schema.hotel_details_request_schema import HotelDetailsRequest
 
 
-async def save_hotelDetails_roomsOption(convo_id, hotelDetails_request: dict):
+
+async def save_hotelDetails_roomsOption(convo_id, hotelDetails_data: dict):
         """        
         Save the hotel details and room options to Redis. 
         Args:
@@ -12,36 +11,36 @@ async def save_hotelDetails_roomsOption(convo_id, hotelDetails_request: dict):
             hotelDetails_request (dict): Hotel details and room options to save.
         """
         hotel_details = { 
-            "id": hotelDetails_request.get("id"),
-            "name": hotelDetails_request.get("name"),
-            "city": hotelDetails_request.get("city"),
-            "country": hotelDetails_request.get("country"),
-            "address": hotelDetails_request.get("address"),
-            "stars": hotelDetails_request.get("stars"),
-            "ratings": hotelDetails_request.get("ratings"),
-            "longitude": hotelDetails_request.get("longitude"),
-            "latitude": hotelDetails_request.get("latitude"),
-            "desc": hotelDetails_request.get("desc"),
-            "img": hotelDetails_request.get("img"),
-            "amenities": hotelDetails_request.get("amenities"),
-            "supplier_name": hotelDetails_request.get("supplier_name"),
-            "supplier_id": hotelDetails_request.get("supplier_id"),
-            "checkin": hotelDetails_request.get("checkin"),
-            "checkout": hotelDetails_request.get("checkout"),
-            "policy": hotelDetails_request.get("policy"),
-            "booking_age_requirement": hotelDetails_request.get(
+            "id": hotelDetails_data.get("id"),
+            "name": hotelDetails_data.get("name"),
+            "city": hotelDetails_data.get("city"),
+            "country": hotelDetails_data.get("country"),
+            "address": hotelDetails_data.get("address"),
+            "stars": hotelDetails_data.get("stars"),
+            "ratings": hotelDetails_data.get("ratings"),
+            "longitude": hotelDetails_data.get("longitude"),
+            "latitude": hotelDetails_data.get("latitude"),
+            "desc": hotelDetails_data.get("desc"),
+            "img": hotelDetails_data.get("img"),
+            "amenities": hotelDetails_data.get("amenities"),
+            "supplier_name": hotelDetails_data.get("supplier_name"),
+            "supplier_id": hotelDetails_data.get("supplier_id"),
+            "checkin": hotelDetails_data.get("checkin"),
+            "checkout": hotelDetails_data.get("checkout"),
+            "policy": hotelDetails_data.get("policy"),
+            "booking_age_requirement": hotelDetails_data.get(
                 "booking_age_requirement"
             ),
-            "cancellation": hotelDetails_request.get("cancellation"),
-            "tax_percentage": hotelDetails_request.get("tax_percentage"),
-            "hotel_phone": hotelDetails_request.get("hotel_phone"),
-            "hotel_email": hotelDetails_request.get("hotel_email"),
-            "hotel_website": hotelDetails_request.get("hotel_website"),
-            "discount": hotelDetails_request.get("discount"),
+            "cancellation": hotelDetails_data.get("cancellation"),
+            "tax_percentage": hotelDetails_data.get("tax_percentage"),
+            "hotel_phone": hotelDetails_data.get("hotel_phone"),
+            "hotel_email": hotelDetails_data.get("hotel_email"),
+            "hotel_website": hotelDetails_data.get("hotel_website"),
+            "discount": hotelDetails_data.get("discount"),
         }
 
         rooms_options = []
-        rooms = hotelDetails_request.get("rooms") or []
+        rooms = hotelDetails_data.get("rooms") or []
         for idx, room in enumerate(rooms):
             rooms_options.append({**room, "option": idx + 1, "number_of_selected": 0})
         try:
