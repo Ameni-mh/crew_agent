64cfb3963a535328ff817d17454fd5ab8a0d3087
Ameni-mh <ameni.m@mediastorykw.com>
2025-08-05 12:35:49 +0100

    add longTerm memory

--- src/main.py
+++ src/main.py
@@ -4,17 +4,23 @@ from langgraph.checkpoint.redis import AsyncRedisSaver
 from langgraph.prebuilt import create_react_agent
 from config.config import settings
 from Agent.lookup_hotels import model, State, prompt, tools, summarization_node
+from langgraph.store.redis.aio import AsyncRedisStore
+
 app = FastAPI()
 
 async def startup_span():
-    async with AsyncRedisSaver.from_conn_string(settings.redis_url) as checkpointer:
-        #checkpointer.setup()
+    async with (
+        AsyncRedisSaver.from_conn_string(settings.redis_url) as checkpointer,
+        AsyncRedisStore.from_conn_string(settings.redis_url) as store):
+        await checkpointer.asetup()
+        await store.setup()
         app.agent =  create_react_agent(
             model=model,
             tools=tools,
             prompt=prompt,
             verbose=True,
             checkpointer=checkpointer,
+            store=store,
             pre_model_hook= summarization_node,
             state_schema=State
             )      
